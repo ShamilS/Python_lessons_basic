17335e219ca478bfb66be335b1df3ea59ec69a51
Shamil Salakhetdinov <Shamil.Salakhetdinov@gmail.com>
2019-02-28 18:19:48 +0300

    hw02 solutions

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -12,14 +12,48 @@
 # 4.  арбуз
 
 # Подсказка: воспользоваться методом .format()
+# https://pyformat.info/
+def EnumerateFruits(fruits):
+    maxlen = 0
+    for fruit in fruits: 
+        if len(fruit) > maxlen: maxlen = len(fruit)
+    formatStr = '{}. {:>' + str(maxlen) + '}'
+    i = 1
+    for fruit in fruits:
+        print (formatStr.format(i, fruit))
+        i = i+1
 
+#test
+EnumerateFruits(["яблоко", "банан", "киви", "арбуз", "watermelon"])
 
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
+def SubstractList(list1, list2):
+    return set(list1) - set(list2)
+
+#test    
+print( \
+     SubstractList ( \
+        ["яблоко", "банан", "киви", "арбуз", "watermelon"], \
+        ["яблоко", "киви", "watermelon", "orange"] \
+    )\
+)
 
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+def ProcessList(list1):
+    list2 = []
+    for x in list1: 
+        if x % 2 == 0:
+            list2.append(x / 4)
+        else:
+            list2.append(x * 2)
+    return list2
+ 
+list1 = [2, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+list2 = ProcessList(list1)
+print(list2)
